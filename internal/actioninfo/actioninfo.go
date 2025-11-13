6736f45dc1fb3744344b381cfce549ddfe00cc84
Дмитрий Степанченко <Dizelis7@yandex.ru>
2025-11-13 20:52:47 +0300

    first commit

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,28 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
-	// TODO: добавить методы
+	Parse(datastring string) (err error)
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+
+	for i, ch := range dataset {
+		err := dp.Parse(ch)
+		if err != nil {
+			log.Printf("Information formation error: %v", err)
+		}
+		i++
+		continue
+	}
+	info, err := dp.ActionInfo()
+	if err != nil {
+		log.Printf("Information formation error%v", err)
+	}
+	fmt.Println(info)
 }
