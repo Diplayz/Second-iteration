39067bb611d19ca97b653beaff1325b57215f007
Дмитрий Степанченко <Dizelis7@yandex.ru>
2025-11-13 21:25:55 +0300

    Sixth commit

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -8,7 +8,7 @@ type Personal struct {
 	Height float64
 }
 
-func (p Personal) Print() string {
-	return fmt.Sprintf("Имя: %s\nВес: %.2f кг.\nРост: %.2f м.\n",
+func (p Personal) Print() {
+	fmt.Printf("Имя: %s\nВес: %.2f кг.\nРост: %.2f м.\n",
 		p.Name, p.Weight, p.Height)
 }
